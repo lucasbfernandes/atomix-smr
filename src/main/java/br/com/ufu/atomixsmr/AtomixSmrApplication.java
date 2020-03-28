41919899c2c8b41510d68c3e877ad0f5edf4e08c
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-03-28 15:04:39 -0300

    Adding project structure

---
+++ src/main/java/br/com/ufu/atomixsmr/AtomixSmrApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufu.atomixsmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomixSmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomixSmrApplication.class, args);
+	}
+
+}
