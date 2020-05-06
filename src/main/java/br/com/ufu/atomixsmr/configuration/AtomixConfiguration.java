7eb0bc27f1b3aa447d38854232302fa281a9c63f
lucasbfernandes <lucasbfernandes94@gmail.com>
2020-05-06 00:02:39 -0300

    Adding service and atomix configuration files

--- src/main/java/br/com/ufu/atomixsmr/configuration/AtomixConfiguration.java
+++ src/main/java/br/com/ufu/atomixsmr/configuration/AtomixConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.*;
+import java.util.Map;
 
 @Configuration
 public class AtomixConfiguration {
@@ -14,10 +15,19 @@ public class AtomixConfiguration {
         final InputStream inputSreamConf = getClass().getClassLoader().getResourceAsStream("atomix.conf");
         final File conf = new File("atomix.conf");
         final OutputStream outputStreamConf = new FileOutputStream(conf);
-        outputStreamConf.write(inputSreamConf.readAllBytes());
 
-        final Atomix atomix = new Atomix(conf);
-        atomix.start().join();
-        return atomix;
+        outputStreamConf.write(inputSreamConf.readAllBytes());
+        Map<String, String> environmentVars  = System.getenv();
+
+        environmentVars.entrySet().forEach(System.out::println);
+
+        try {
+            final Atomix atomix = new Atomix(conf);
+            atomix.start().join();
+            return atomix;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
